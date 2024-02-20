d6aef9afd35e38df78754b19f3f9caae63bcabda
Roosevelt Franklin <roosevelt_franklin@hotmail.com>
2024-02-20 17:08:21 -0300

    adicionando nova tarefa

--- src/paginas/adicionar-tarefa/adicionarTarefa.jsx
+++ src/paginas/adicionar-tarefa/adicionarTarefa.jsx
@@ -1,10 +1,41 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
 function AdicionarTarefa() {
+
+  const [tarefa, setTarefa] = useState({
+    id: 0,
+    descricao: ''
+  })
+
+  const navigate = useNavigate()
+
+  const handleChange = (event) => {
+    setTarefa({
+      ...tarefa,
+      [event.target.name]: event.target.value
+    })
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const response = axios.post('http://localhost:3000/tarefas', tarefa);
+    console.log(response.data);
+    alert('Nova tarefa adicionada com sucesso!');
+    navigate('/');
+  }
 
     return (
       <>
-        <h1>
-          Adicionar nova tarefa
-        </h1>
+        <h1>Adicionar nova tarefa</h1>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Digite sua tarefa:
+            <input type="text" name="descricao" value={tarefa.descricao} onChange={handleChange} />
+          </label>
+          <button type="submit">Adicionar</button>
+        </form>
       </>
     )
   }
--- src/paginas/listar-tarefas/listarTarefas.jsx
+++ src/paginas/listar-tarefas/listarTarefas.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect} from 'react'
+import { Link } from 'react-router-dom'
 import axios from 'axios'
 function ListarTarefas() {
 
@@ -17,6 +18,7 @@ function ListarTarefas() {
             <li key={tarefa.id}>{tarefa.descricao}</li>
           ))}
         </ul>
+        <Link to={'/adicionar-tarefa'}>Adicionar nova tarefa</Link>
       </>
     )
   }
