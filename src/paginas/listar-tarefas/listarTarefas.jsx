968fb729c0ee6d4710be2b22fbd0a62a79ac2daa
Roosevelt Franklin <roosevelt_franklin@hotmail.com>
2024-02-20 16:50:26 -0300

    Carregando a lista na tela

--- src/paginas/listar-tarefas/listarTarefas.jsx
+++ src/paginas/listar-tarefas/listarTarefas.jsx
@@ -1,10 +1,22 @@
+import { useState, useEffect} from 'react'
+import axios from 'axios'
 function ListarTarefas() {
 
+  const [tarefas, setTarefas] = useState([])
+  useEffect(() => {
+    axios.get('http://localhost:3000/tarefas')
+    .then(response => {
+      setTarefas(response.data)
+    })
+  }, [])
     return (
       <>
-        <h1>
-          Minha todo-list
-        </h1>
+        <h1>Minha todo-list</h1>
+        <ul>
+          {tarefas.map(tarefa => (
+            <li key={tarefa.id}>{tarefa.descricao}</li>
+          ))}
+        </ul>
       </>
     )
   }
